409d9b36974e08fe424e6a5700c4c7472e916a9d
irfanzidniofficial <irfanzidniofficial@gmail.com>
2024-06-10 10:41:10 +0700

    add package module from go-say-hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/irfanzidniofficial/app-say-hello
+
+go 1.22.4
+
+require github.com/irfanzidniofficial/go-say-hello v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/irfanzidniofficial/go-say-hello"
+)
+
+func main() {
+	hello:=go_say_hello.SayHello()
+
+	fmt.Println(hello)
+}
