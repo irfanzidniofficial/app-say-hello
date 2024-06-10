b988c3cc113a224ccec65049f23fb85af82a52d9
irfanzidniofficial <irfanzidniofficial@gmail.com>
2024-06-10 11:04:56 +0700

    major update package module say-hello v1.5.0 to v2.0.0 add parameter name in module

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/irfanzidniofficial/app-say-hello
 
 go 1.22.4
 
-require github.com/irfanzidniofficial/go-say-hello v1.5.0
+require github.com/irfanzidniofficial/go-say-hello/v2 v2.0.0
--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 
-	go_say_hello "github.com/irfanzidniofficial/go-say-hello"
+	go_say_hello "github.com/irfanzidniofficial/go-say-hello/v2"
 )
 
 func main() {
-	hello:=go_say_hello.SayHello()
+	hello:=go_say_hello.SayHello("Irfan")
 
 	fmt.Println(hello)
 }
